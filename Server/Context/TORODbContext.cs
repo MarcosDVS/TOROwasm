a60d79d8d82144bdcd7b4f8ab4487ca2f6c8b740
Marcos De Vargas Serrano <117132296+MarcosDVS@users.noreply.github.com>
2023-02-11 13:01:21 -0400

    Creando la conexion a la base de datos

--- Server/Context/TORODbContext.cs
+++ Server/Context/TORODbContext.cs
@@ -11,6 +11,11 @@ internal interface ITORODbContext
 {
     DbSet<Usuario> Usuarios { get; set; }
     DbSet<UsuarioRol> UsuariosRoles { get; set; }
+    DbSet<Bovino> Bovinos { get; set; }
+    DbSet<Padre> Padres { get; set; }
+    DbSet<Madre> Madres { get; set; }
+    DbSet<Preñes> Preñeses { get; set; }
+    DbSet<Produccion> Producciones { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
@@ -31,6 +36,11 @@ internal class TORODbContext : DbContext, ITORODbContext
     #region Tablas de la BD.
     public DbSet<Usuario> Usuarios { get; set; } = null!;
     public DbSet<UsuarioRol> UsuariosRoles { get; set; } = null!;
+    public DbSet<Bovino> Bovinos { get; set; } = null!;
+    public DbSet<Padre> Padres { get; set; } = null!;
+    public DbSet<Madre> Madres { get; set; } = null!;
+    public DbSet<Preñes> Preñeses { get; set; } = null!;
+    public DbSet<Produccion> Producciones { get; set; } = null!;
 
     #endregion
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
