09a6e4f39d90c793ce01d850a6c09fe01d4276d1
Marcos De Vargas Serrano <117132296+MarcosDVS@users.noreply.github.com>
2023-02-19 14:08:39 -0400

    results del proyecto

---
+++ Shared/Wrapper/Result.cs
@@ -0,0 +1,83 @@
+
+namespace TORO.Shared.Wrapper;
+
+public class Result
+{
+    public Result()
+    {
+        
+    }
+    public Result(bool succeeded, List<string>? message = default!)
+    {
+        Succeeded = succeeded;
+        Message = message;
+    }
+
+    public bool Succeeded { get; set; }
+    public List<string>? Message { get; set; }
+
+    public static Result Fail()
+    {
+        return new Result(false);
+    }
+    public static Result Fail(string message)
+    {
+        return new Result(false, new List<string>(){ message});
+    }
+    public static Result Fail(List<string> message)
+    {
+        return new Result(false, message);
+    }
+
+   public static Result Success(string message)
+    {
+        return new Result(true, new List<string>(){ message});
+    }
+    public static Result Success(List<string> message)
+    {
+        return new Result(true, message);
+    } 
+}
+
+public class Result<T>:Result
+{
+    public T Data { get; set; } = default!;
+
+    public new static Result<T> Fail()
+    {
+        return new Result<T>(){Succeeded = false, Message = new List<string>(){"❌"}};
+    }
+    public new static Result<T> Fail(string message)
+    {
+        return new Result<T>(){Succeeded = false, Message = new List<string>(){message}};
+    }
+    public new static Result<T> Fail(List<string> messages)
+    {
+        return new Result<T>(){Succeeded = false, Message = messages};
+    }
+
+    public static Result<T> Success(T data)
+    {
+        return new Result<T>(){
+            Succeeded = true, 
+            Message = new List<string>(){"👌"},
+            Data = data
+            };
+    }
+    public static Result<T> Success(T data, string message)
+    {
+        return new Result<T>(){
+            Succeeded = true, 
+            Message = new List<string>(){"👌"},
+            Data = data
+            };
+    }
+    public static Result<T> Success(T data, List<string> messages)
+    {
+        return new Result<T>(){
+            Succeeded = true, 
+            Message = messages,
+            Data = data
+            };
+    }
+}
